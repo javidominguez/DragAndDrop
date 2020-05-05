3dc22b7d8a7da730f112e4d66a22ec83faab71e5
Javi Dominguez <javidominguez_github@outlook.es>
2020-05-05 11:22:21 +0200

    Added turkish localization by Cagri Dogan.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Makes it easy to drag and drop objects."""),
 	# version
-	"addon_version" : "2.0dev",
+	"addon_version" : "2.0.1dev",
 	# Author(s)
 	"addon_author" :  u"Javi Dominguez <fjavids@gmail.com>",
 	# URL for the add-on documentation support
